e0000860459ec2968953e50de7d04476c9111b7b
Glória Braz <gloria.liviabraz@gmail.com>
2025-07-21 14:29:33 -0300

    📚 Aula 15 | 34
    03/07/2025

---
+++ 02-ControleVersao-IntroducaoFrontEnd/aula15-EventosInteratividade/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const navToggle = document.getElementById('navToggle');
+    const navMenu = document.getElementById('navMenu');
+    
+    navToggle.addEventListener('click', () => {
+        const isOpen = navMenu.classList.toggle('open');
+        navToggle.setAttribute('aria-expanded', isOpen);
+
+        navToggle.innerHTML = isOpen ? '&times' : '&#9776;';
+        navToggle.setAttribute('aria-label', isOpen ? 'Fechar menu' : 'Abrir menu');
+    });
+})
